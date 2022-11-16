84b2ca70e57d43fffd63f8e6b9a6053bf4d02ebd
yogitheboss <yograjrjpt123@gmail.com>
2022-11-16 21:46:05 +0530

    added problem statement link

--- array_problem/maxArea_water.py
+++ array_problem/maxArea_water.py
@@ -10,7 +10,7 @@
 # example:Input: height = [1,8,6,2,5,4,8,3,7]
 # Output: 49
 # Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-
+#problem link:https://leetcode.com/problems/container-with-most-water/description/
 
 #solution:
 def maxArea(height) -> int:
