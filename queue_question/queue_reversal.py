c4076751e9a89389444343ddff9ca758901aaadc
yogitheboss <yograjrjpt123@gmail.com>
2022-11-19 01:17:20 +0530

    added reverse queue problem

---
+++ queue_question/queue_reversal.py
@@ -0,0 +1,43 @@
+# Day 
+#QUEUE REVERSAL
+
+#Problem link:https://practice.geeksforgeeks.org/problems/queue-reversal/1
+# Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the function rev(), that reverses the N elements of the queue.
+
+# Example 1:
+# Input:
+# 6
+# 4 3 1 10 2 6
+
+# Output: 
+# 6 2 10 1 3 4
+
+# Explanation: 
+# After reversing the given
+# elements of the queue , the resultant
+# queue will be 6 2 10 1 3 4.
+
+from queue import Queue
+
+def rev(q):
+    Stack = []
+    #adding all value of queue in stack and empting the queue    
+    while (not q.empty()):
+        Stack.append(q.get())
+    #readding value from stack to queue but now as stack will pop from last we put last values first
+    while (Stack):
+        q.put(Stack.pop())
+        
+    return q
+
+q=Queue(maxsize=4)
+q.put('a')
+q.put('b')
+q.put('c')
+q.put('d')
+
+print('reversal before queue',q.queue)
+q=rev(q)
+print('after reversal queue',q.queue)
+
+
