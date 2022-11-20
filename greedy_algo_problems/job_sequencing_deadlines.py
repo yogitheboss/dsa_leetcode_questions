f418733d208a7ac7ebe1cb06e537f1bdef244791
yogitheboss <yograjrjpt123@gmail.com>
2022-11-20 16:48:58 +0530

    modified job seq deadline algo

--- greedy_algo_problems/job_sequencing_deadlines.py
+++ greedy_algo_problems/job_sequencing_deadlines.py
@@ -1,19 +1,22 @@
 #Job sequencing based on deadline
-profits=[35,30,25,20,15,12,5]
-deadlines=[3,4,4,2,3,1,2]
+profits=[20,15,10,5,1]
+deadlines=[2,2,1,3,3]
 total_slots=max(deadlines)
 
 #making slots for assigning jobs
 allot_array=[0]*total_slots
-
 max_profit=0
-for i in range(total_slots):
+i=0
+#checking if all slots occupied or no more jobs ready to sequence
+while(total_slots>0 and i<len(profits)):
     for j in reversed(range(deadlines[i])):
         #if slot empty then add the job to it and update the profit
         if(allot_array[j]==0):
             allot_array[j]=1
             max_profit+=profits[i]
+            total_slots-=1
             break
+    i+=1
 print(max_profit)
 
 
