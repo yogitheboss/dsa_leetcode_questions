eb7c95a9bf334a5a2ebeaab4f936d893ebf497ee
yogitheboss <yograjrjpt123@gmail.com>
2022-11-23 00:08:53 +0530

    word search update fixed tle

--- backtracing/word_search.py
+++ backtracing/word_search.py
@@ -10,32 +10,31 @@
 #problem link:https://leetcode.com/problems/word-search/description/
 
 
-def exist(board, word):
-	w=len(word)
-	row=len(wordlist)
-	col=len(wordlist[0])
-    #set to store previous positions
-	store=set()
-	def dfs(r,c,i):
-		if(i==w):
-			return True
-        # check if indexes outof bound, coming from previous place or char isn't equal
-		if(r>row-1 or c>col-1 or r<0 or c<0 or (r,c) in store or word[i]!=wordlist[r][c]):
-			return False
-        # storing previous position
-		store.add((r,c))
-		res=(dfs(r+1,c,i+1) or dfs(r,c+1,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1))
-        #backtrack previous position
-		store.remove((r,c))
-		return res
+def exist(wordlist, word: str) -> bool:
+        w=len(word)
+        row=len(wordlist)
+        col=len(wordlist[0])
+        
+        def dfs(r,c,i):
+            if(i==w):
+                return True
+            #checking for outofbound or previous match or char doesn't match
+            if(r>row-1 or c>col-1 or r<0 or c<0 or wordlist[r][c]=='.'or word[i]!=wordlist[r][c]):
+                return False
+            temp=wordlist[r][c]
+            #making a marker if passed a place
+            wordlist[r][c]='.'
+            res=(dfs(r+1,c,i+1) or dfs(r,c+1,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1))
+            #backtrack to previous state
+            wordlist[r][c]=temp
+            return res
 
 
-	for j in range(row):
-		for k in range(col):
-            #call for each char
-			if dfs(j,k,0):
-				return True
-	return False
+        for j in range(row):
+            for k in range(col):
+                if dfs(j,k,0):
+                    return True
+        return False
 wordlist=[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
 word="ABCB"
 print(exist(wordlist,word))
